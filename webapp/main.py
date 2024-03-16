bac5fe53b6228ea526d7b602278894e7f32a72a3
damien-martinez1 <105747194+damien-martinez1@users.noreply.github.com>
2024-03-16 01:57:04 +0000

    Added pydantic model

--- webapp/main.py
+++ webapp/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+from hashlib import sha256
 
 current_dir = dirname(abspath(__file__))
 static_path = join(current_dir, "static")
@@ -35,3 +36,15 @@ def generate(body: Body):
     """
     string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
     return {'token': string}
+
+# create a pydantic model for me 
+
+
+class Token(BaseModel):
+    token: str
+
+    @app.post('/checksum')
+    def calculate_checksum(body: Body):
+        text = body.text
+        checksum = sha256(text.encode()).hexdigest()
+        return {'checksum': checksum}
